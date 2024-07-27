9585e49a0213a3bab0623e0b04787513b5969f5f
allexjasper <alexander.jasper@gmx.net>
2024-07-27 09:27:41 +0200

    Git ignore for Windwos

---
+++ msvc/oil_pump2/oil_pump2.cpp
@@ -0,0 +1,48 @@
+// oil_pump2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+//#include <boost/asio.hpp>
+#include <boost/regex.hpp>
+#include <chrono>
+#include <mutex>
+#include <fstream>
+#include <optional>
+#include <deque>
+#include <boost/lexical_cast.hpp>
+#include <tuple>
+#include <boost/program_options.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/trivial.hpp>
+//#include <windows.h>
+#include <numbers>
+
+#include <SDL.h>
+/*
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!\n";
+}
+*/
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
